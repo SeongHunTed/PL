ca1cbe4905ee9a7fb31783366f8b1ad76f9f6598
SeongHunTed <4047ksh@naver.com>
2022-05-26 01:20:49 +0900

    PL3 : Manage Mode Done

--- Assign3/TozzBookingSystem.c
+++ Assign3/TozzBookingSystem.c
@@ -17,11 +17,13 @@
 //      스터디 공간 사용 인원 : 1 ~ 10
 //      스터디 공간 : 이름, 층, 책상 개수, 보유 컴퓨터 개수 임의 지정
 
-int managerMode();  // 관리자 모드
-int manageRoom(FILE *fp); // 지점별 스터디 공간 관리
-int deleteBranch(FILE *fp);
-int bemptycheck(FILE *fp, int branchNum);
-int remptycheck(FILE *fp, int roomNum, int branchNum);
+int managerMode();                                      // 관리자 모드
+int insertBranch(FILE *fp);                             // 지점 추가
+int manageRoom(FILE *fp);                               // 지점별 스터디 공간 관리
+int deleteBranch(FILE *fp);                             // 지점 삭제
+int bemptycheck(FILE *fp, int branchNum);               // 지점 존재 확인
+int remptycheck(FILE *fp, int roomNum, int branchNum);  // 스터디 공간 존재 확인
+
 
 // int userMode();     // 사용자 모드
 
@@ -62,6 +64,7 @@ int main(){
     return 0;
 }
 
+// 관리자 모드
 int managerMode(){
 
     FILE *fp;
@@ -76,32 +79,13 @@ int managerMode(){
     printf("\n\n [1] 지점 추가 \n [2] 지점 수정 \n [3] 지점 삭제 \n [4] 초기화면 \n\n\n");
     printf(" Option : ");
     scanf("%d", &select);
-    int branchNum;
-    int buf = 0;
+    
 
     switch (select)
     {
     case 1:
         printf("\n\n <지점 추가> \n\n");
-        printf(" 추가 하실 지점 번호를 입력하세요 : ");
-        scanf("%d", &branchNum);
-            
-        fseek(fp, (branchNum - 1) * RECORDSIZE, SEEK_SET);
-        fread(&buf, sizeof(int), 1, fp);
-        
-        if(branchNum > 6){
-            printf(" 잘못된 접근입니다!\n");
-        }
-
-        if(branchNum != buf){
-            fseek(fp, (branchNum - 1) * RECORDSIZE, SEEK_SET);
-            fwrite(&branchNum, sizeof(int), 1, fp);
-            fclose(fp);
-        } else {
-            printf(" 이미 존재하는 지점 입니다! \n");
-            return 0;
-        }
-
+        insertBranch(fp);
         break;
         
     case 2:
@@ -122,6 +106,33 @@ int managerMode(){
 
 }
 
+// 지점 생성
+int insertBranch(FILE *fp){
+    int branchNum;
+    int buf = 0;
+    printf(" 추가 하실 지점 번호를 입력하세요 : ");
+    scanf("%d", &branchNum);
+            
+    fseek(fp, (branchNum - 1) * RECORDSIZE, SEEK_SET);
+    fread(&buf, sizeof(int), 1, fp);
+        
+    if(branchNum > 6){
+        printf(" 잘못된 접근입니다!\n");
+    }
+
+    if(branchNum != buf){
+        fseek(fp, (branchNum - 1) * RECORDSIZE, SEEK_SET);
+        fwrite(&branchNum, sizeof(int), 1, fp);
+        fclose(fp);
+        return 1;
+    } else {
+        printf(" 이미 존재하는 지점 입니다! \n");
+        fclose(fp);
+        return 0;
+    }
+}
+
+// 스터디 공간 관리
 int manageRoom(FILE *fp){
 
     int select = 0;     // 관리자 옵션 값
@@ -138,7 +149,7 @@ int manageRoom(FILE *fp){
     case 1:
         printf("\n\n <스터디 공간 추가> \n\n");
         
-        printf(" 스터디 공간을 추가할 지점 번호를 입력하세요 : ");
+        printf(" 스터디 공간을 추가할 ||!지점 번호!||를 입력하세요 : ");
         scanf("%d", &branchNum);
 
         if(branchNum > 6){
@@ -187,6 +198,76 @@ int manageRoom(FILE *fp){
         fclose(fp);
         break;
     
+    case 2:
+        printf("\n\n <스터디 공간 수정> \n\n");
+        printf(" 스터디 공간을 수정할 ||!지점 번호!||를 입력하세요");
+        scanf("%d", &branchNum);
+        if(branchNum > 6){
+            printf(" 잘못된 접근입니다!\n");
+        }
+
+        // 지점이 존재 하지 않을 경우 
+        if(bemptycheck(fp, branchNum) == 0) {
+            printf(" 존재하지 않는 지점 입니다!\n");
+            return 0;
+        }
+
+        printf(" 수정할 스터디 공간의 번호를 입력하세요 : ");
+        scanf("%d", &roomNum);
+
+        if(roomNum > 5){
+            printf(" 잘못된 접근입니다!\n");
+            return 0;
+        }
+
+        printf(" 스터디 공간의 허용 인원을 수정 해주세요 : ");
+        scanf("%d", &maxNum);
+
+        if(maxNum > 10){
+            printf(" 잘못된 접근입니다!\n");
+            return 0;
+        }
+
+        if(remptycheck(fp, roomNum, branchNum) == 0) {
+            printf(" 존재하지 않은 스터디 공간 입니다!\n");
+            return 0;
+        }
+
+        // 최대 인원 재설정
+        fseek(fp, 8 + (roomNum-1)*ROOMRECORD + (branchNum-1)*RECORDSIZE, SEEK_SET);
+        fwrite(&maxNum, sizeof(int), 1, fp);
+
+        // 공간 정보 동기화
+        sprintf(recordbuf, "%d번 지점의 %d번 방 || %d인 실 || 컴퓨터 보유 : %d ", branchNum, roomNum, maxNum, maxNum);
+        fseek(fp, 12 + (roomNum-1)*ROOMRECORD + (branchNum-1)*RECORDSIZE, SEEK_SET);
+        fwrite(recordbuf, sizeof(recordbuf), 1, fp);
+
+        fclose(fp);
+        break;
+    
+    case 3:
+        printf("\n\n <스터디 공간 삭제> \n\n");
+        int deleteRoom = 0;
+        int zero = 0;
+        char emptyrecord[ROOMRECORD] = {0};
+        printf("\n\n 삭제할 스터디 공간의 ||!지점번호!||를 입력하세요 : ");
+        scanf("%d", &branchNum);
+        printf("\n\n 삭제할 스터디 공간의 번호를 입력하세요 : ");
+        scanf("%d", &deleteRoom);
+
+        if(remptycheck(fp, deleteRoom, branchNum) == 0){
+            printf(" 존재하지 않는 지점입니다!\n");
+            return 0;
+        }
+
+        fseek(fp, 4 + (deleteRoom-1)*ROOMRECORD + (branchNum-1) * RECORDSIZE, SEEK_SET);
+        fwrite(&zero, sizeof(int), 1, fp);
+        fseek(fp, 8 + (deleteRoom-1)*ROOMRECORD + (branchNum-1) * RECORDSIZE, SEEK_SET);
+        fwrite(&zero, sizeof(int), 1, fp);
+        fseek(fp, 4 + (deleteRoom-1)*ROOMRECORD + (branchNum-1) * RECORDSIZE, SEEK_SET);
+        fwrite(emptyrecord, ROOMRECORD, 1, fp);
+        fclose(fp);
+        break;
     default:
         printf(" Wrong Option!\n");
         break;
@@ -194,8 +275,10 @@ int manageRoom(FILE *fp){
     return 0;
 }
 
+// 지점 삭제
 int deleteBranch(FILE *fp){
     int deleteBranch = 0;
+    int zero = 0;
     char emptyrecord[RECORDSIZE] = {0};
     printf("\n\n 삭제할 지점의 번호를 입력 하세요 : ");
     scanf("%d", &deleteBranch);
@@ -205,6 +288,8 @@ int deleteBranch(FILE *fp){
         return 0;
     }
 
+    fseek(fp, (deleteBranch-1)*RECORDSIZE, SEEK_SET);
+    fwrite(&zero, sizeof(int), 1, fp);
     fseek(fp, (deleteBranch-1)*RECORDSIZE, SEEK_SET);
     fwrite(emptyrecord, RECORDSIZE, 1, fp);
 
@@ -230,6 +315,8 @@ int remptycheck(FILE *fp, int roomNum, int branchNum){
     fseek(fp, 4 + (roomNum-1)*ROOMRECORD + (branchNum-1)*RECORDSIZE, SEEK_SET);
     fread(&buf, sizeof(int), 1, fp);
 
+    // 0 -> 존재 하지 않았던 경우
+    // 1 -> 이미 존재하는 경우
     if(buf != roomNum){
         return 0;
     } else return 1;
